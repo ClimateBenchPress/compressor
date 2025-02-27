b55dd68d3fc3922f7956345cf0f79f0a91223830
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-02-27 10:12:12 +0200

    Add the tthresh codec (#9)

--- src/climatebenchpress/compressor/compressors/__init__.py
+++ src/climatebenchpress/compressor/compressors/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ["BitRound", "BitRoundPco", "StochRound", "Sz3", "Zfp"]
+__all__ = ["BitRound", "BitRoundPco", "StochRound", "Sz3", "Tthresh", "Zfp"]
 
 from . import abc as abc
 from .bitround import BitRound
 from .bitround_pco import BitRoundPco
 from .stochround import StochRound
 from .sz3 import Sz3
+from .tthresh import Tthresh
 from .zfp import Zfp
