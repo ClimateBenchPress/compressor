d1460ee5c99795a2ce5060003c17137ea2dabdfe
Tim Reichelt <treichelt6@gmail.com>
2025-03-12 13:27:07 +0000

    Create main function

--- scripts/print_metrics.py
+++ scripts/print_metrics.py
@@ -4,13 +4,19 @@ import pandas as pd
 
 REPO = Path(__file__).parent.parent
 
-all_results = pd.read_csv(REPO / "metrics" / "all_results.csv")
-for dataset in all_results["Dataset"].unique():
-    print("\n" + 100 * "=")
-    print(f"Results on {dataset}")
-    print(100 * "=" + "\n")
-    print(
-        all_results[all_results["Dataset"] == dataset]
-        .drop(columns=["Dataset"])
-        .to_markdown(index=False)
-    )
+
+def main():
+    all_results = pd.read_csv(REPO / "metrics" / "all_results.csv")
+    for dataset in all_results["Dataset"].unique():
+        print("\n" + 100 * "=")
+        print(f"Results on {dataset}")
+        print(100 * "=" + "\n")
+        print(
+            all_results[all_results["Dataset"] == dataset]
+            .drop(columns=["Dataset"])
+            .to_markdown(index=False)
+        )
+
+
+if __name__ == "__main__":
+    main()
