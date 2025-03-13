2028490433985444ff498c3fce417f67aef72106
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-03-13 17:18:32 +0200

    Upgrade numcodecs-wasm-zfp to v0.4.0 (#14)
    * Upgrade numcodecs-wasm-zfp to v0.4.0
    
    * Remove numcodecs-wasm-swizzle-reshape dependency

--- src/climatebenchpress/compressor/compressors/zfp.py
+++ src/climatebenchpress/compressor/compressors/zfp.py
@@ -1,9 +1,7 @@
 __all__ = ["Zfp"]
 
-import numcodecs_wasm_swizzle_reshape
 import numcodecs_wasm_zfp
 from numcodecs.abc import Codec
-from numcodecs_combinators.stack import CodecStack
 
 from .abc import Compressor
 
@@ -14,10 +12,4 @@ class Zfp(Compressor):
 
     @staticmethod
     def build() -> Codec:
-        return CodecStack(
-            # collapse into ((realization, time,), (vertical,), (latitude,), (longitude,))
-            numcodecs_wasm_swizzle_reshape.SwizzleReshape(
-                axes=[[0, 1], [2], [3], [4]],
-            ),
-            numcodecs_wasm_zfp.Zfp(mode="fixed-accuracy", tolerance=0.01),
-        )
+        return numcodecs_wasm_zfp.Zfp(mode="fixed-accuracy", tolerance=0.01)
