abfa94988ca4d3a27453a326ac5e061bd56626ae
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-03-31 11:01:40 +0300

    Add a Jpeg2000 compressor (#17)
    * Add a Jpeg2000 compressor
    
    * Fix formatting
    
    * Update numcodecs-wasm-jpeg2000 to v0.1.1
    
    * Ducktape over deadlock
    
    * Bump numcodecs-wasm to v0.1.3 to fix deadlocks

--- src/climatebenchpress/compressor/compressors/__init__.py
+++ src/climatebenchpress/compressor/compressors/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ["BitRound", "BitRoundPco", "StochRound", "Sz3", "Tthresh", "Zfp"]
+__all__ = ["BitRound", "BitRoundPco", "Jpeg2000", "StochRound", "Sz3", "Tthresh", "Zfp"]
 
 from . import abc as abc
 from .bitround import BitRound
 from .bitround_pco import BitRoundPco
+from .jpeg2000 import Jpeg2000
 from .stochround import StochRound
 from .sz3 import Sz3
 from .tthresh import Tthresh
