b304f8fa086a8224e5e8950fc220003b9219be58
Tim Reichelt <treigerm@users.noreply.github.com>
2025-04-11 14:20:44 +0100

    Test whether provided error bound is satisfied (#20)
    * Add test to check whether error bound is satisfied
    
    * Explicit string casting to make mypy happy
    
    * Make error check inclusive
    
    * Fix relative error bound bug
    
    Co-authored-by: Juniper Tyree <50025784+juntyr@users.noreply.github.com>
    
    * Add check that float parsing is successfull
    
    * Add float type assertion and infinity checks
    
    * Switch to logical operators to make mypy happy
    
    ---------
    
    Co-authored-by: Juniper Tyree <50025784+juntyr@users.noreply.github.com>

---
+++ src/climatebenchpress/compressor/tests/error_bound.py
@@ -0,0 +1,41 @@
+import numpy as np
+import xarray as xr
+
+from .abc import Test
+
+
+class ErrorBound(Test):
+    def __init__(self, error_type: str, threshold: float = 0.05):
+        self.threshold = threshold
+        assert error_type in [
+            "abs_error",
+            "rel_error",
+        ], f"error_type must be either 'abs_error' or 'rel_error', not {error_type}"
+        self.error_type = error_type
+
+    def __call__(self, x: xr.DataArray, y: xr.DataArray) -> tuple[bool, float]:
+        # Check that two arrays are both floats
+        assert x.dtype.kind == "f", f"Expected x to be float, got {x.dtype}"
+        assert y.dtype.kind == "f", f"Expected y to be float, got {y.dtype}"
+
+        abs_error = np.abs(x - y)
+        relative_error = abs_error / abs(x)
+
+        error_to_check = abs_error if self.error_type == "abs_error" else relative_error
+        satisfied = error_to_check <= self.threshold
+
+        # The comparison does not work for NaN values, as `np.nan < threshold` is False.
+        # This check ensures that if x contains a NaN then y must also contain a NaN at
+        # the same location.
+        # Note, it is an error to have a NaN in y and not in x which will be caught.
+        x_and_y_nan = np.isnan(x) & np.isnan(y)
+        satisfied = satisfied | x_and_y_nan
+
+        # Similarly, np.inf - np.inf is NaN but should pass the test.
+        # The x == y condition ensures that their sign is the same.
+        x_and_y_inf = np.isinf(x) & np.isinf(y) & x == y
+        satisfied = satisfied | x_and_y_inf
+
+        # Proportion of entries that exceed the threshold.
+        exceed_thresh = np.sum(~satisfied) / x.size
+        return bool(exceed_thresh == 0.0), float(exceed_thresh)
