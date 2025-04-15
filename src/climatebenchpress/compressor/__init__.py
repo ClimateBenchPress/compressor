eb653f76e94976847c5119e4ff471d4091941821
Juniper Tyree <juniper.tyree@helsinki.fi>
2025-04-15 10:11:39 +0300

    Move scripts into the package

--- src/climatebenchpress/compressor/__init__.py
+++ src/climatebenchpress/compressor/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["compressors", "metrics", "tests"]
+__all__ = ["compressors", "metrics", "scripts", "tests"]
 
 from . import cf as cf
-from . import compressors, metrics, tests
+from . import compressors, metrics, scripts, tests
