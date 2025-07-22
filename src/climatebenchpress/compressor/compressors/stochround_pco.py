c80fed1875c4876c6b1eb16c6bb18656c6cb05d9
Juniper Tyree <juniper.tyree@helsinki.fi>
2025-07-22 10:22:58 +0300

    Fix precision and rounding for stochround codec

--- src/climatebenchpress/compressor/compressors/stochround_pco.py
+++ src/climatebenchpress/compressor/compressors/stochround_pco.py
@@ -16,7 +16,7 @@ class StochRoundPco(Compressor):
     def abs_bound_codec(error_bound, **kwargs):
         precision = error_bound
         return CodecStack(
-            numcodecs_wasm_uniform_noise.UniformNoise(scale=precision / 2, seed=42),
+            numcodecs_wasm_uniform_noise.UniformNoise(scale=precision, seed=42),
             numcodecs_wasm_round.Round(precision=precision),
             numcodecs_wasm_pco.Pco(
                 level=8,
