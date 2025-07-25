3e954761ae142cdae283e96f5f968952cec8907b
Tim Reichelt <treichelt6@gmail.com>
2025-07-25 12:10:47 +0100

    ZFP relative error through expert mode

--- src/climatebenchpress/compressor/compressors/zfp.py
+++ src/climatebenchpress/compressor/compressors/zfp.py
@@ -3,6 +3,7 @@ __all__ = ["Zfp"]
 import numcodecs_wasm_zfp_classic
 
 from .abc import Compressor
+from .utils import NONMANTISSA_BITS, compute_keepbits
 
 
 class Zfp(Compressor):
@@ -21,4 +22,22 @@ class Zfp(Compressor):
     def abs_bound_codec(error_bound, **kwargs):
         return numcodecs_wasm_zfp_classic.ZfpClassic(
             mode="fixed-accuracy", tolerance=error_bound
+        )
+
+    # NOTE:
+    # ZFP mechanism for strictly supporting relative error bounds is to
+    # apply bitshaving and then use ZFP's lossless mode for compression.
+    # See https://zfp.readthedocs.io/en/release1.0.1/faq.html#q-relerr for more details.
+    @staticmethod
+    def rel_bound_codec(error_bound, *, dtype=None, **kwargs):
+        assert dtype is not None, "dtype must be provided"
+
+        mantissa_keepbits = compute_keepbits(dtype, error_bound)
+        total_keepbits = mantissa_keepbits + NONMANTISSA_BITS[dtype]
+        return numcodecs_wasm_zfp_classic.ZfpClassic(
+            mode="expert",
+            min_bits=0,
+            max_bits=0,
+            max_prec=total_keepbits,
+            min_exp=-1075,
         )
