306ccda335a8e8863d878688a179ab0e524f1817
Juniper Tyree <juniper.tyree@helsinki.fi>
2025-08-13 20:20:15 +0300

    Switch to fused stochastic rounding codec

--- src/climatebenchpress/compressor/compressors/stochround_pco.py
+++ src/climatebenchpress/compressor/compressors/stochround_pco.py
@@ -1,8 +1,7 @@
 __all__ = ["StochRoundPco"]
 
 import numcodecs_wasm_pco
-import numcodecs_wasm_round
-import numcodecs_wasm_uniform_noise
+import numcodecs_wasm_stochastic_rounding
 from numcodecs_combinators.stack import CodecStack
 
 from .abc import Compressor
@@ -16,8 +15,9 @@ class StochRoundPco(Compressor):
     def abs_bound_codec(error_bound, **kwargs):
         precision = error_bound
         return CodecStack(
-            numcodecs_wasm_uniform_noise.UniformNoise(scale=precision, seed=42),
-            numcodecs_wasm_round.Round(precision=precision),
+            numcodecs_wasm_stochastic_rounding.StochasticRounding(
+                precision=precision, seed=42
+            ),
             numcodecs_wasm_pco.Pco(
                 level=8,
                 mode="auto",
