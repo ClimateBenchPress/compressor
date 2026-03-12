7101d790ad7a08e381bb8fdaca3e4bf9b8755058
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-03-12 09:46:46 +0200

    Bump numcodecs-random-projection to v0.1.0a5

--- src/climatebenchpress/compressor/compressors/rp_dct.py
+++ src/climatebenchpress/compressor/compressors/rp_dct.py
@@ -17,3 +17,36 @@ class RPDct(Compressor):
             max_block_memory=2**28,  # 256 MiB
             debug=True,
         )
+
+
+def create_rp_dct_cr_compressor(cr: float) -> type[Compressor]:
+    class RPDctCr(Compressor):
+        name = f"rp-dct-{cr}"
+        description = f"Random Projection (DCT) with CR={cr}"
+
+        @staticmethod
+        def abs_bound_codec(error_bound, **kwargs):
+            return numcodecs_random_projection.RPCodec(
+                cr=cr,
+                method="dct",
+                max_block_memory=2**28,  # 256 MiB
+                debug=True,
+            )
+
+        @staticmethod
+        def rel_bound_codec(error_bound, **kwargs):
+            return numcodecs_random_projection.RPCodec(
+                cr=cr,
+                method="dct",
+                max_block_memory=2**28,  # 256 MiB
+                debug=True,
+            )
+
+    return RPDctCr
+
+
+create_rp_dct_cr_compressor(2.0)
+create_rp_dct_cr_compressor(5.0)
+create_rp_dct_cr_compressor(10.0)
+create_rp_dct_cr_compressor(50.0)
+create_rp_dct_cr_compressor(100.0)
